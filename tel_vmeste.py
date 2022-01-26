fbc86b87d145d74bd86fc233ea944bfc16aac575
Temik667 <sarbupin1t@gmail.com>
2022-01-26 15:16:43 +0600

    крч добавил TOKEN и теперь там User ID выскакивает если написать /start

--- tel_vmeste.py
+++ tel_vmeste.py
@@ -1,22 +1,74 @@
-import telebot
-from telebot import types
 
-API_KEY = '5073950568:AAHaViAz4s_2b-UcuY7jB2t9KcdWgUkIEoE'
-bot = telebot.TeleBot(API_KEY)
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
 
-@bot.message_handler(commands = ['start'])
-# def start(message):
-#   bot.reply_to(message, 'How it is going?')
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-def start(message):
-  markup = types.ReplyKeyboardMarkup()
-  buttonA = types.KeyboardButton('Cоздать')
-  buttonB = types.KeyboardButton('B')
-  buttonC = types.KeyboardButton('C')
+import logging
+import keys
 
-  markup.row(buttonA, buttonB)
-  markup.row(buttonC)
+from telegram import Update, ForceReply, Contact
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-  bot.send_message(message.chat.id, 'It works!', reply_markup=markup)
-  
-bot.polling()
+key = keys.API_KEY
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.message.from_user
+    update.message.reply_text('Your Name: {}\nPhone: {}\nID: {}'.format(user['username'], user['phone_number'], user['id']))
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(key)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
